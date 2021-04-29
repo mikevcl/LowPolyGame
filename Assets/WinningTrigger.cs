0ae281e164f51efdb5c9e23b3aea3ca71f18afb6
mikevcl <michaelvancleavelopez@gmail.com>
2021-04-28 19:39:16 -0700

    Huge Update
    Winning goal added as helicopter behind military tents
    
    zombie damage to player added
    
    winning game screen with credits added
    
    edited demo map to include about 60 zombies to scene
    
    added some point lights and directional lights to the scene for better visuals
    
    added a few sounds to the zombies while attacking and dying
    
    added audio system that handles playing multiple audio files

---
+++ Assets/WinningTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class WinningTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    private bool wonGame = false;
+
+    void OnTriggerEnter()
+    {
+        gameManager.CompleteLevel();
+        wonGame = true;
+    }
+    public bool getWin()
+    {
+        return wonGame;
+    }
+}
--- Assets/scripts/enemy/ZombieController.cs
+++ Assets/scripts/enemy/ZombieController.cs
@@ -7,12 +7,25 @@ using UnityEngine.UI;
 public class ZombieController : MonoBehaviour
 {
 	public healthSystem zombieHealth = new healthSystem(100);
+	//public playerHealth playerH = new playerHealth(100);
 	public Slider healthBar;
 	//
 	public float lookRadius = 10f;
 	Animator animator;
 	Transform target;
 	NavMeshAgent agent;
+	//[Header("Audio Clips")]
+	//[Tooltip("The audio clip that is played while zombie is idle."), SerializeField]
+	//private AudioSource z_idling;
+
+	//[Tooltip("The audio clip that is played while zombie is running."), SerializeField]
+	//private AudioSource z_running;
+
+	//[Tooltip("The audio clip that is played while zombie is dying."), SerializeField]
+	//private AudioSource z_dying;
+
+	//[Tooltip("The audio clip that is played while zombie is attacking."), SerializeField]
+	//private AudioSource zombieNoises;
 
 	void Start()
 	{
@@ -22,27 +35,40 @@ public class ZombieController : MonoBehaviour
 		animator = GetComponent<Animator>();
 		target = FindingPlayer.instance.player.transform;
 		agent = GetComponent<NavMeshAgent>();
+
+		
+		//zombieNoises = GetComponent<AudioSource>();
+		/*
+		z_idling = GetComponent<AudioSource>();
+		z_dying = GetComponent<AudioSource>();
+		z_running = GetComponent<AudioSource>();
+		*/
 	}
 
 	void Update()
 	{
+		if (target == null || target.position == null || transform.position == null)
+			return;
 		float distance = Vector3.Distance(target.position, transform.position);
 
 
 		if (zombieHealth.getHealth() <= 1)
 		{
+			//z_dying.Play();
 			animator.SetBool("isDead", true);
+			FindObjectOfType<AudioManager>().Play("z_dying");
 			StartCoroutine(wait());
 			wait();
 			enabled = false;
 		}
 		//if player is inside look radius and isn't in attacking distance then run
-		if(distance > lookRadius)
+		if(distance < lookRadius)
 			animator.SetBool("isRunning", true);
-		if (distance <= lookRadius && distance>agent.stoppingDistance)
+		if (distance <= lookRadius && distance > agent.stoppingDistance)
 		{
 			animator.SetBool("isRunning", true);
 			distance = Vector3.Distance(target.position, transform.position);
+			//z_running.Play();
 			if (distance > lookRadius)
 				agent.SetDestination(transform.position);
 			else
@@ -56,14 +82,19 @@ public class ZombieController : MonoBehaviour
 			//Debug.Log("running false");
 		}
 		//if player is within attacking distance then attack
-		if (distance <= (agent.stoppingDistance + .3))
+		if (distance <= (agent.stoppingDistance+.3))
 		{
-			
+			//zombieNoises.Play();
 			LookAtPlayer();
+			//z_attacking.Play();
 			
 			animator.SetBool("isAttacking", true);
+			//StartCoroutine(waitAttAud());
+			//waitAttAud();
+			
 			//put damage to player from zombie here
 			//Debug.Log("attacking true");
+			//DamagePlayer(10);
 		}
 		// else set attacking to false
 		else
@@ -73,7 +104,14 @@ public class ZombieController : MonoBehaviour
 		}
 		
 	}
-
+	/*
+	void DamagePlayer(int damage)
+	{
+		playerH.playerHP.Damage(damage);
+		playerH.healthBar.value = playerH.playerHP.getHealth();
+		Debug.Log("Health:" + playerH.playerHP.getHealth());
+	}
+	*/
 	void LookAtPlayer()
 	{
 		Vector3 direction = (target.position - transform.position).normalized;
@@ -91,7 +129,7 @@ public class ZombieController : MonoBehaviour
 	{
 		zombieHealth.Damage(damage);
 		healthBar.value = zombieHealth.getHealth();
-		Debug.Log("Zombie health:" + zombieHealth.getHealth()) ;
+		Debug.Log("Zombie health:" + zombieHealth.getHealth());
 		/*if (zombieHealth.getHealth() == 0)
 		{
 			//wait(15);
@@ -106,4 +144,15 @@ public class ZombieController : MonoBehaviour
 		Destroy(this.gameObject);
 
 	}
+	/*
+	public IEnumerator waitAttAud()
+	{
+
+		Debug.Log(Time.time);
+		FindObjectOfType<AudioManager>().Play("z_attacking");
+		yield return new WaitForSeconds(3);
+		
+	}
+	*/
+
 }
